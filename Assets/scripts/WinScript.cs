09cd474c3df7b53ad8339b9eb74d6539e20d1c86
LewyDew <lewhollopeter@gmail.com>
2016-11-12 17:52:54 -0600

    added WinScript and WinText, win() function has to be called in the inventory.

---
+++ Assets/scripts/WinScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class WinScript : MonoBehaviour {
+	public InputScript input;
+	public TextMesh text;
+	public int waitTime;
+	public void win()
+	{
+		GetComponent<TextMesh> ().text = "Player " + (input.playerNumber + 1) + " Wins";
+		StartCoroutine (restart());
+	}
+	IEnumerator restart()
+	{
+		yield return new WaitForSeconds (waitTime);
+		string currentSceneName = SceneManager.GetActiveScene ().name;
+		SceneManager.LoadScene (currentSceneName);
+	}
+
+}
