59fa2bdb883c75340371dfccdbd2eefdbbd4a7cb
unknown <whittefang@gmail.com>
2016-11-11 21:04:12 -0600

    player input, movement, prefab, health, inventory

---
+++ Assets/scripts/HealthScrit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScrit : MonoBehaviour {
+	public int currentHealth = 0;
+	public delegate void voidDel();
+	public voidDel deathFunction;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void DealDamage(int amount){
+		currentHealth -= amount;
+		if (currentHealth <= 0) {
+			deathFunction ();
+		}
+	}
+
+}
---
+++ Assets/scripts/PlayerInventory.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour {
+
+	Item currentItem;
+	int currentIngredients;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void AddIngredient(){
+		currentIngredients++;
+	}
+	public void RemoveIngredients(){
+		currentIngredients = 0;
+	}
+	public void ChangeItem(Item newItem){
+		if (currentItem != Item.none) {
+			// eject current item
+		}
+		currentItem = newItem;
+	}
+}
+
+
+public enum Item{
+	Bomb,
+	Torch,
+	Whip,
+	Buckets,
+	none
+}
